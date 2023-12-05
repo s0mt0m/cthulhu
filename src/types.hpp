f8638662c33b9498f5a1c592ad426cfac86ab23d
somtom <tomaskrchnak@azet.sk>
2023-12-05 16:48:36 +0100

    cthu: Add some helper checks and convertors between types

--- src/types.hpp
+++ src/types.hpp
@@ -25,5 +25,40 @@ namespace cthu
         _count_
     };
 
+    constexpr word builtin_count = static_cast< word >( builtin_type::_count_ );
+
+    template< typename T > constexpr word type_id {};
+    template<> constexpr word type_id< void >  = word( builtin_type::none );
+    template<> constexpr word type_id< byte >  = word( builtin_type::byte );
+    template<> constexpr word type_id< half >  = word( builtin_type::half );
+    template<> constexpr word type_id< word >  = word( builtin_type::word );
+    template<> constexpr word type_id< stck >  = word( builtin_type::stck );
+    template<> constexpr word type_id< dict >  = word( builtin_type::dict );
+
+    template< builtin_type > struct bitvec_type : std::false_type {};
+    template<> struct bitvec_type< builtin_type::byte > : std::true_type { using type = byte; };
+    template<> struct bitvec_type< builtin_type::half > : std::true_type { using type = half; };
+    template<> struct bitvec_type< builtin_type::word > : std::true_type { using type = word; };
+
+    template< builtin_type > struct signed_type : std::false_type {};
+    template<> struct signed_type< builtin_type::byte > : std::true_type { using type = int8_t; };
+    template<> struct signed_type< builtin_type::half > : std::true_type { using type = int16_t; };
+    template<> struct signed_type< builtin_type::word > : std::true_type { using type = int32_t; };
+
+    constexpr word stid_bits( word opcode ) { return opcode >> 20; }
+
+    struct program;
+
+    template< typename tag_t, builtin_type type >
+    struct op
+    {
+        static bool apply( program &, word )
+        {
+            UNREACHABLE();
+        }
+    };
+
+    struct exec_tag {};
+
 } // namespace cthu
 
