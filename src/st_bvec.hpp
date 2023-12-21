bb01fc8a39f4235e100788f66744e77981dd01de
somtom <tomaskrchnak@azet.sk>
2023-12-21 22:50:04 +0100

    cthu: Implement the rest of binary operations

--- src/st_bvec.hpp
+++ src/st_bvec.hpp
@@ -9,7 +9,8 @@ namespace cthu
         requires bitvec_type< type_id >::value
     struct op< exec_tag, type_id >
     {
-        using value_t = typename bitvec_type< type_id >::type;
+        using value_t  = typename bitvec_type< type_id >::type;
+        using signed_t = std::make_signed_t< value_t >;
 
         template< typename impl_t >
         static bool binary( program &p, word opcode, impl_t impl )
@@ -25,16 +26,39 @@ namespace cthu
         {
             switch ( bvec_op( opcode ) )
             {
-                #define BINARY( OP ) binary( p, opcode, \
+                #define BINARY_U( OP ) binary( p, opcode, \
                     []( value_t a, value_t b ) { return a OP b; } )
 
-                case bvec_add<>: return BINARY( + );
-                case bvec_sub<>: return BINARY( - );
-                case bvec_mul<>: return BINARY( * );
+                #define BINARY_S( OP ) binary( p, opcode, \
+                    []( signed_t a, signed_t b ) { return a OP b; } )
 
-               #undef BINARY
+                case bvec_add<>:  return BINARY_U( + );
+                case bvec_sub<>:  return BINARY_U( - );
+                case bvec_mul<>:  return BINARY_U( * );
+                case bvec_udiv<>: return BINARY_U( / );
+                case bvec_urem<>: return BINARY_U( % );
+                case bvec_sdiv<>: return BINARY_S( / );
+                case bvec_srem<>: return BINARY_S( % );
 
-               default: UNREACHABLE( opcode, bvec_op( opcode ) );
+                case bvec_and<>:  return BINARY_U( & );
+                case bvec_or<>:   return BINARY_U( | );
+                case bvec_xor<>:  return BINARY_U( ^ );
+
+                case bvec_eq<>:   return BINARY_U( == );
+                case bvec_neq<>:  return BINARY_U( != );
+                case bvec_ult<>:  return BINARY_U( <  );
+                case bvec_ule<>:  return BINARY_U( <= );
+                case bvec_ugt<>:  return BINARY_U( >  );
+                case bvec_uge<>:  return BINARY_U( >= );
+                case bvec_slt<>:  return BINARY_S( <  );
+                case bvec_sle<>:  return BINARY_S( <= );
+                case bvec_sgt<>:  return BINARY_S( >  );
+                case bvec_sge<>:  return BINARY_S( >= );
+
+                #undef BINARY_U
+                #undef BINARY_S
+
+                default: UNREACHABLE( opcode, bvec_op( opcode ) );
             }
         }
     };
