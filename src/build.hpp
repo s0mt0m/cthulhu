6037ed7b710cdff89404592540ea2f738fdd64a4
somtom <tomaskrchnak@azet.sk>
2023-12-21 13:41:23 +0100

    cthu: Remove topo-sort and building

--- src/build.hpp
+++ src/build.hpp
@@ -99,102 +99,25 @@ namespace cthu::builder
         friend stack_builder;
 
         std::vector< stack > stacks;
-        std::vector< stack_proxy > stack_proxies;
         std::vector< dictionary > dicts;
 
         word stack_id = 0;
         word dict_id = 0;
     };
 
-    enum class color : uint8_t { WHITE, GREY, BLACK };
-
-    static void topo_sort( ref curr, auto &stacks, auto &out, auto &marks )
-    {
-        if ( marks[ curr ] == color::BLACK )
-            return;
-
-        if ( marks[ curr ] == color::GREY )
-            ASSERT( false ); // cycles are not allowed
-
-        marks[ curr ] = color::GREY;
-
-        for ( auto [ pos, next ] : stacks[ curr.ref_id ].refs )
-            topo_sort( next, stacks, out, marks );
-
-        marks[ curr ] = color::BLACK;
-        out.push_back( curr );
-    }
-
-    // u -> v, v is before u
-    static std::vector< ref > topo_sort( auto &stacks )
-    {
-        const auto count = stacks.size();
-
-        std::vector< ref > sorted;
-        sorted.reserve( count );
-
-        std::map< ref, color > marks;
-        for ( word i = 0; i < count; ++i )
-            marks[ stack_ref( i ) ] = color::WHITE;
-
-        for ( word i = 0; i < count; ++i )
-            topo_sort( stack_ref( i ), stacks, sorted, marks );
-
-        return sorted;
-    }
-
     auto program::build( std::vector< stack_proxy > init )
     {
         ASSERT( init.size() <= 8 );
 
         cthu::program p;
-        std::map< word, word > program_ids;
-
-        stack_builder sb( *this );
-
-        for ( auto i : topo_sort( stacks ) )
-        {
-            auto id = sb.create( p, i );
-            program_ids.emplace( stacks[ i.ref_id ].id, id );
-        }
-
-        std::vector< word > initials;
-
-        for ( stack_proxy s : init )
-            initials.push_back( program_ids[ s.id ] );
-
-        for ( word i = init.size(); i < 8; ++i )
-            initials.push_back( p.add_stack( {} ) );
-
-        p.set_inits( initials );
 
         return p;
     }
 
     word stack_builder::create( cthu::program &out, ref id )
     {
-        std::vector< stack_builder::info_t > new_blueprint;
-
-        const auto new_stack = [ this, &out ]( const auto &step )
-        {
-            auto [ dst, pos, src ] = step;
-            const auto sid = out.add_stack( { p.stacks[ src.ref_id ].data } );
-            p.stacks[ dst.ref_id ].data[ pos ] = sid;
-        };
-
-        for ( auto [ pos, ref ] : p.stacks[ id.ref_id ].refs )
-        {
-            ASSERT( blueprints.contains( ref ) );
-            const auto &steps = blueprints[ ref ];
-
-            for ( const auto &step : steps )
-                new_stack( step );
-
-            append_range( new_blueprint, steps );
-            new_blueprint.emplace_back( id, pos, ref );
-
-            new_stack( new_blueprint.back() );
-        }
+        return 0;
+    }
 
         blueprints.emplace( id, std::move( new_blueprint ) );
         return out.add_stack( { p.stacks[ id.ref_id ].data } );
