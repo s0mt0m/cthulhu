931926ed8ec80a483c3d59572d21582652dd3d44
somtom <tomaskrchnak@azet.sk>
2024-01-16 14:16:33 +0100

    cthu: Add function that gets all arguments in a tuple

--- src/op_bvec.hpp
+++ src/op_bvec.hpp
@@ -21,6 +21,15 @@ namespace cthu
             return ( opcode >> ( 4 * idx ) ) & utils::ones< 4 >;
     }
 
+    template< word count = 3 >
+    constexpr auto bvec_args( word opcode )
+    {
+        return [ = ]< word... idx >( std::integer_sequence< word, idx... > )
+        {
+            return std::make_tuple( stck_arg< idx >( opcode )... );
+        }( std::make_integer_sequence< word, count >{} );
+    }
+
     consteval word make_opcode( word stid, word opid )
     {
         if ( stid == 0 )
