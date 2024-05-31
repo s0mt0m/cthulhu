0a8059b0495beb8260dec88c5f921559c2386f2a
somtom <me@somtom.eu>
2024-05-31 12:30:03 +0200

    cthu: Add pragram class

--- src/program.hpp
+++ src/program.hpp
@@ -19,6 +19,19 @@ namespace cthu
         {
             slots[ pos ].push( value );
         }
+
+        word add_stack( stack s )
+        {
+            stacks.push_back( std::move( s ) );
+            return stacks.size() - 1;
+        }
+
+        void set_inits( const std::vector< word > &ids )
+        {
+            ASSERT( ids.size() == 8 );
+            for ( word i = 0; i < 8; ++i )
+                slots[ i ] = std::move( stacks[ ids[ i ] ] );
+        }
     };
 
 } // namespace cthu
